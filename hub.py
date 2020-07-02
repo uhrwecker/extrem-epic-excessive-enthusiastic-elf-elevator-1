fc2a057b53005735f1f4d402cdeeca944a5d04d0
FabianPiwowarczyk <67393931+FabianPiwowarczyk@users.noreply.github.com>
2020-07-02 22:10:59 +0200

    added function to setup game

--- hub.py
+++ hub.py
@@ -1,11 +1,7 @@
 from classes.playerc import playerclass as pc
+import start_setup as setup
 
-player_1 = pc.Player('human', 'mage')
-player_2 = pc.Player('orc', 'fighter')
+players = setup.start_setup()
 
-player_1.change_stat("HP", 1)
-
-print(player_1.HP)
-
-print("Player 1:","\n", player_1.stat_display())
-print("Player 2:","\n", player_2.stat_display())
+print("Player 1:","\n", players[0].stat_display())
+print("Player 2:","\n", players[1].stat_display())
